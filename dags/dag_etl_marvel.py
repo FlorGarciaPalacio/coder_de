520f1622df37458a377255ce7cf2ecde6dbf94b8
FlorGarciaPalacio <109027060+FlorGarciaPalacio@users.noreply.github.com>
2024-09-05 04:19:38 -0300

    Update dag_etl_marvel.py

--- dags/dag_etl_marvel.py
+++ dags/dag_etl_marvel.py
@@ -1,12 +1,13 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from APIS.functions import *
+from APIS.functions import extract_response,transform_data
+import os
 
 default_args={
     'owner': 'FlorGP',
-    'retries':5,
-    'retry_delay': timedelta(seconds=30)
+    'retries':2,
+    'retry_delay': 2
 }
 
 with DAG(
@@ -14,13 +15,17 @@ with DAG(
     dag_id='dag_api_marvel_endpoint_characters',
     description= 'Dag que extrae informacion de la API marvel endpoint characters',
     start_date=datetime(2024,9,1),
-    schedule_interval=timedelta(days=1),
-    catchup=False
+    schedule="0 0 * * *",
+    catchup=True
     ) as dag:
     task_extract = PythonOperator(
         task_id='Extraer-data-API',
-        python_callable=print(),
-        op_args=['Hola Mundo']
+        python_callable=extract_response,
+        op_args=["characters",os.getcwd()]
         )
-
-    task_extract
+    task_transform = PythonOperator(
+        task_id='transformar-data-API',
+        python_callable=transform_data,
+        op_args=[os.getcwd()]
+        )
+task_extract >> task_transform
